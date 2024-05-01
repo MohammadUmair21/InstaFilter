ece497094c50dc964c136f47123dfbd191c7ad2b
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-05-01 14:26:09 +0530

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Umair on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
